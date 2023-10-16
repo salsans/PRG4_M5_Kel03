e28980b71f54a136760af3b7cdf0ec591d65f2ae
salsans <salsans46@gmail.com>
2023-10-16 10:43:14 +0700

    Mengunggah file

---
+++ PRG4_M5_KEL03/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRG4_M5_KEL03.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
